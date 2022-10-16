b73dec61e867e3fac1ae69bbf1d07fcc626afcc1
Joshua Joseph <joshua841015@gmail.com>
2022-10-16 23:11:00 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (15.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // RECURSION
+//     RECURSION
 //     int fib(int n) {
         
 //         if(n==0){
@@ -15,22 +15,37 @@ public:
 //     }
     
 // TOP DOWN DP 
-    int topDown(int n, vector<int> &dp){
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n] = topDown(n-1, dp) + topDown(n-2, dp);
-        return dp[n];
-    }
+//     int topDown(int n, vector<int> &dp){
+//         if(n<=1){
+//             return n;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         dp[n] = topDown(n-1, dp) + topDown(n-2, dp);
+//         return dp[n];
+//     }
+    
+//     int fib(int n) {
+//         vector<int> dp(n+1);
+//         for(int i=0; i<=n; i++){
+//             dp[i]=-1;
+//         }
+//         return topDown(n, dp);
+//     }
+    
+    // BOTTOM UP DP 
     
     int fib(int n) {
         vector<int> dp(n+1);
-        for(int i=0; i<=n; i++){
-            dp[i]=-1;
+        if(n<=1){
+            return n;
         }
-        return topDown(n, dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
