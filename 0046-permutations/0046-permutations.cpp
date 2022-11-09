10421c4c0920fb1a722293f21c01a133e6313844
Joshua Joseph <joshua841015@gmail.com>
2022-11-09 18:21:01 +0530

    Time: 4 ms (68.91%), Space: 7.6 MB (91.86%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
-//     void solve(vector<int>& nums, int index, vector<vector<int>>& ans){
+    void solve(vector<int>& nums, int index, vector<vector<int>>& ans){
         
-//         if(index>=nums.size()){
-//             ans.push_back(nums);
-//             return ;
-//         }
-        
-//         for(int j = index; j<nums.size(); j++){
-//             swap(nums[index], nums[j]);
-//             solve(nums, index+1, ans);
-//             swap(nums[index], nums[j]);
-//         }
-        
-        
-//     }
-    
-//     vector<vector<int>> permute(vector<int>& nums) {
-        
-//         vector<vector<int>> ans;
-//         solve(nums, 0, ans);
-//         return ans;
-        
-//     }
-    void solve(vector<int>& nums, int index, vector<vector<int>>& ans, 
-               unordered_map<int, bool> &check, vector<int> res){
         if(index>=nums.size()){
-            ans.push_back(res);
+            ans.push_back(nums);
             return ;
         }
-        for(int i=0; i<nums.size(); i++){
-            if(!check[nums[i]]){
-                check[nums[i]]=true;
-                res.push_back(nums[i]);
-                solve(nums, index+1, ans, check, res);
-                res.pop_back();
-                check[nums[i]]=false;
-            }
+        
+        for(int j = index; j<nums.size(); j++){
+            swap(nums[index], nums[j]);
+            solve(nums, index+1, ans);
+            swap(nums[index], nums[j]);
         }
+        
+        
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_map<int, bool> check;
+        
         vector<vector<int>> ans;
-        vector<int> res;
-        solve(nums, 0, ans, check, res);
-        return ans; 
+        solve(nums, 0, ans);
+        return ans;
+        
     }
+    
+    // SPACE COMPLEXITY IS HIGH AS COMPARED TO THE ABOVE SOLUTION
+//     void solve(vector<int>& nums, int index, vector<vector<int>>& ans, 
+//                unordered_map<int, bool> &check, vector<int> res){
+//         if(index>=nums.size()){
+//             ans.push_back(res);
+//             return ;
+//         }
+//         for(int i=0; i<nums.size(); i++){
+//             if(!check[nums[i]]){
+//                 check[nums[i]]=true;
+//                 res.push_back(nums[i]);
+//                 solve(nums, index+1, ans, check, res);
+//                 res.pop_back();
+//                 check[nums[i]]=false;
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         unordered_map<int, bool> check;
+//         vector<vector<int>> ans;
+//         vector<int> res;
+//         solve(nums, 0, ans, check, res);
+//         return ans; 
+//     }
 };
