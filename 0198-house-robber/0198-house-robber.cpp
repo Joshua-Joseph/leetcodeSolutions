b083a1259f2d88d5401a3fbaaa707372e47963fb
Joshua Joseph <joshua841015@gmail.com>
2022-12-14 10:41:52 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,14 +4,24 @@ public:
         if(nums.size()==1){
             return nums[0];
         }
-        vector<int> dp(nums.size(), 0);
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
+        // vector<int> dp(nums.size(), 0);
+        // dp[0] = nums[0];
+        // dp[1] = max(dp[0], nums[1]); // max((dp[i-1]+0), (dp[i-2]+nums[i]))
+        // for(int i=2; i<nums.size(); i++){
+        //     int inc = dp[i-1] + 0;
+        //     int exc = dp[i-2] + nums[i];
+        //     dp[i] = max(inc, exc);
+        // }
+        // return dp[nums.size()-1];
+        int prev2 = nums[0];
+        int prev1 = max(prev2, nums[1]);
         for(int i=2; i<nums.size(); i++){
-            int inc = dp[i-1] + 0;
-            int exc = dp[i-2] + nums[i];
-            dp[i] = max(inc, exc);
+            int inc = prev1 + 0;
+            int exc = prev2 + nums[i];
+            int ans = max(inc, exc);
+            prev2 = prev1;
+            prev1 = ans;
         }
-        return dp[nums.size()-1];
+        return prev1;
     }
 };
