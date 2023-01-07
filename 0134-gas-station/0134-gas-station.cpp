4dd0a27683ed17d05c92c630465814ca46d53335
Joshua Joseph <joshua841015@gmail.com>
2023-01-07 20:45:24 +0530

    Time: 87 ms (86.76%), Space: 69.4 MB (55.78%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = 0;
+        int bal = 0;
+        int def = 0;
+        for (int i=0; i<gas.size(); i++){
+            bal = bal + gas[i] - cost[i];
+            if(bal<0){
+                def = def + bal;
+                start = i+1;
+                bal = 0;
+            }
+        }
+        if(bal+def>=0){
+            return start;
+        }
+        else{
+            return -1;
+        }
+    }
+};
